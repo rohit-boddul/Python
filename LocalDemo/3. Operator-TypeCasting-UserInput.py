e345bb9569ec7d3271fb12f69533a1ec1830ff2b
rohit-boddul <rohitnboddul@gmail.com>
2025-11-15 13:00:47 +0530

    Updated - 15 Nov

--- LocalDemo/3. Operator-TypeCasting-UserInput.py
+++ LocalDemo/3. Operator-TypeCasting-UserInput.py
@@ -1,4 +1,5 @@
 from loguru import logger
+import math
 
 length_of_land = 100
 breadth_of_land = 100
@@ -9,3 +10,54 @@ is_home = True
 #1. Calculate total area of the land? 
 total_area_of_land = length_of_land * breadth_of_land
 logger.info(f"Total area of the land is {total_area_of_land} sq.ft.")
+
+#2. Perimeter of land?
+perimeter_of_land = 2 * (length_of_land + breadth_of_land)
+logger.info(f"Perimeter of the land is {perimeter_of_land}m")
+
+#3. Modulo Operator (gives remainder - ARMSTRONG Number Code)
+logger.info (15%6)
+
+#4. Division (gives division values in FLOAT - even if it a whole number)
+logger.info(15/6)
+
+#5. Floor Division (gives Quotient)
+logger.info(15//6)
+
+#6. Ceiling values 
+logger.info(math.ceil(15/6)) 
+# -----------------------------------------------------------------------------------------------------------
+
+
+# TYPE CONVERSION
+
+a = "25"
+b = 25
+logger.info(float(a)+b)
+
+# Types of TYPE CASTING
+# i. explicit - developer gives it explicitly 
+# ii. implict - python itself does it by its own
+
+# ----------------------------------------------------------------------------------------------------------
+
+# USER INPUT --> 
+
+
+
+#1. area of the circle
+logger.info("To calculate the Area of circle, please provide below values -")
+radius_of_circle = float(input("Radius of the circle - "))
+logger.info(type(radius_of_circle))
+area_of_circle = 3.14159 * float (radius_of_circle)
+logger.info(f"Area of cirle = {area_of_circle} units sq.")
+
+
+
+
+
+
+
+
+
+
